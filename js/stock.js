671176f49ee4402dfae2b3d021d3bbce8325dd1c
Chantal Yukelson <chantalyukelson@MacBook-Air-de-Chantal.local>
2023-05-08 23:05:32 -0300

    preentrega 2

---
+++ js/stock.js
@@ -0,0 +1,8 @@
+let Servicios = [
+    {id: 1, nombre: "Camisas", precio:15000, cantidad:1, img: 'https://ae01.alicdn.com/kf/S537433d8a897490a8a08d8bd0e759d4aD/Camisa-de-vestir-a-la-moda-para-hombre-camisa-de-manga-larga-sin-hierro-antiarrugas-el.jpg_Q90.jpg_.webp'},
+    {id: 2, nombre: "Pantalones", precio:20000, cantidad:1, img: 'https://i.pinimg.com/550x/4c/5a/93/4c5a9395b035495bdb2a7af7af23d28a.jpg'},
+    {id: 3, nombre: "Zapatos", precio:40000, cantidad:1, img: 'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/118/233/products/rej0850ne-9e36cdb0bd52613ce716799283630224-640-0.webp'},
+    {id: 4, nombre: "Cinturones", precio:8000, cantidad:1, img: 'https://http2.mlstatic.com/D_NQ_NP_813284-MLA52667697974_112022-O.jpg'},
+    {id: 5, nombre: "Ropa elegante", precio:80000, cantidad:1, img: 'https://m.media-amazon.com/images/I/615GdLm+QwL._AC_UX522_.jpg'},
+    {id: 6, nombre: "Tapados", precio:60000, cantidad:1, img: 'https://ae01.alicdn.com/kf/HTB1d3KhNXXXXXc8aXXXq6xXFXXXC.jpg'},
+]
